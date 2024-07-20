a8b6168cdf65cdc9dc4a72341cb940d4128d0ef4
Subhajit Gorai <47049949+sbhjt-gr@users.noreply.github.com>
2024-07-20 22:49:59 +0530

    Update espnetwork.py

--- esp32_files/espnetwork.py
+++ esp32_files/espnetwork.py
@@ -4,7 +4,7 @@ wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 if not wlan.isconnected():
     print('connecting to network...')
-    wlan.connect('sanju-home router-2', '1234543200axR2')
+    wlan.connect('', '')
     while not wlan.isconnected():
         print('.....')
         sleep_ms(100)
