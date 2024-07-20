cbec6d3a8cbbc3d18831804b966e217c336883d8
Subhajit Gorai <47049949+sbhjt-gr@users.noreply.github.com>
2024-07-20 22:54:44 +0530

    Update settings.py

--- Gas_Leakage_Detection/settings.py
+++ Gas_Leakage_Detection/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_q(um&4dp@o_(p%vw3fxivq_%%ss#tle1fn0*jvm%h2$yy8qj-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
