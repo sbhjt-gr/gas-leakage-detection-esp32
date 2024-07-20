32dac6cd1e9ab05f5d4d55906e62e11d85577edc
Subhajit Gorai <47049949+sbhjt-gr@users.noreply.github.com>
2024-07-20 22:51:38 +0530

    Update sms.py

--- esp32_files/sms.py
+++ esp32_files/sms.py
@@ -2,7 +2,7 @@ import urequests as rq
 def sms(gas_val):
     url = "https://www.fast2sms.com/dev/bulkV2"
     message = "Alert!! Gas Leakage Detected! Current Leakage Value: "
-    payload = f"sender_id=TXTIND&message={message}{gas_val}&route=v3&numbers=7047179573"
+    payload = f"sender_id=TXTIND&message={message}{gas_val}&route=v3&numbers="
     headers = {
         'authorization': "",
         'Content-Type': "application/x-www-form-urlencoded",
@@ -13,11 +13,11 @@ def sms(gas_val):
     return response.json()
 
 def wamessage(gas_val):
-    url = f"https://api.callmebot.com/whatsapp.php?phone=917047179573&text=Gas+Leakage+Detected!+Current+Value:+{gas_val}&apikey=4130129"
+    url = f"https://api.callmebot.com/whatsapp.php?phone=&text=Gas+Leakage+Detected!+Current+Value:+{gas_val}&apikey="
     data = rq.get(url).text
     return data
 
 def telegram_call(gas_val):
-    url = f"https://api.callmebot.com/start.php?user=@s_gorai&text=Gas+Leakage+Detected!+Current+Value:+{gas_val}"
+    url = f"https://api.callmebot.com/start.php?user=&text=Gas+Leakage+Detected!+Current+Value:+{gas_val}"
     data = rq.get(url).text
     return data
